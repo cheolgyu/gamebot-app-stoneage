41b240ba87d305863e5be73c39754c464bb502d2
cheolgyu <rb7480@knou.ac.kr>
2020-06-27 09:07:43 +0900

    클릭이벤트 발생을 위해 shell script 테스트 성공

--- app/src/main/java/com/example/myapplication/ShellExecuter.kt
+++ app/src/main/java/com/example/myapplication/ShellExecuter.kt
@@ -1,28 +1,33 @@
 package com.example.myapplication
 
+import android.util.Log
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.lang.StringBuilder
 
 class ShellExecuter {
 
     fun Executer(command:String ):String{
         var output: StringBuffer  = StringBuffer()
         var  p: Process? =null
-        var line : String  = ""
+        var str :StringBuilder =  StringBuilder()
         try {
             p = Runtime.getRuntime().exec(command)
             p.waitFor()
             var reader: BufferedReader  = BufferedReader( InputStreamReader(p.getInputStream()))
 
             while (true) {
-                output.append(line + "\n");
-                reader.readLine()?:break
+                var _line =  reader.readLine()
+                str.append(_line );
+                _line?:break
             }
 
         }catch ( e:Exception){
-            e.printStackTrace()
+           // e.printStackTrace()
+            Log.e("", "122====================================="+e.printStackTrace().toString())
+            return ""
         }
-        return line
+        return str.toString()
     }
 
 
