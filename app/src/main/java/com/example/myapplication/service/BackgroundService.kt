db137c4827e62eb9ea71328890c2b4282d9b8642
cheolgyu <rb7480@knou.ac.kr>
2020-06-30 17:00:14 +0900

    캡쳐후 포인트 리턴 까진 되는데 되다 꺼짐.

--- app/src/main/java/com/example/myapplication/service/BackgroundService.kt
+++ app/src/main/java/com/example/myapplication/service/BackgroundService.kt
@@ -1,38 +1,77 @@
 package com.example.myapplication.service
 
+import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Bitmap.CompressFormat
+import android.graphics.PixelFormat
+import android.graphics.Point
+import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
+import android.media.Image
+import android.media.ImageReader
+import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
 import android.os.*
 import android.util.Log
+import android.view.Display
+import android.view.WindowManager
 import android.widget.Toast
-import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.annotation.RequiresApi
 import com.example.myapplication.MediaProjectionActivity
 import com.example.myapplication.ShellExecuter
 import com.example.myapplication.notification.Noti
 import com.example.myapplication.tflite.Run
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.nio.ByteBuffer
 
 
 class BackgroundService : Service() {
     companion object {
-        var RUN_IMG: String? = null
         var Run = false
         val TAG: String = "BackgroundService"
         private val FOREGROUND_SERVICE_ID = 1000
         private var serviceLooper: Looper? = null
         private var serviceHandler: ServiceHandler? = null
-
+        private var cap_filename: String? = null
         private var handlerThread: HandlerThread? = null
-        var mediaProjectionManager: MediaProjectionManager? = null
 
-        fun newService(context: Context,action :String ): Intent =
-            Intent(context, BackgroundService::class.java).apply {
-                Run = true
-                if (action == "stop"){
+        var context: Context? = null
+        var my_action: String? = null
+
+        var my_data: Intent? = null
+        var my_resultCode: Int? = null
+
+        fun newService(_context: Context,_action :String ): Intent =
+            Intent(_context, BackgroundService::class.java).apply {
+                my_action= _action
+                context = _context
+                if (_action == "start" || _action=="createVirtualDisplay"){
+                    Run = true
+                }else if (_action == "stop"){
                     Run = false
                 }
+            }
+        fun newService(_context: Context,_action :String ,filename: String): Intent =
+            Intent(_context, BackgroundService::class.java).apply {
+                my_action= _action
+                context = _context
+                cap_filename = filename
+                Run = true
+            }
+
+        fun newService(_context: Context,_action :String ,resultCode :Int,data:Intent): Intent =
+            Intent(_context, BackgroundService::class.java).apply {
+                my_data = data
+                my_resultCode = resultCode
+                my_action= _action
+                context = _context
+                //cap_bitmap = bitmap
+                Run = true
             }
     }
 
@@ -42,35 +81,27 @@ class BackgroundService : Service() {
     private inner class ServiceHandler(looper: Looper) : Handler(looper) {
 
         override fun handleMessage(msg: Message) {
-            if(msg.arg2 == 0){
-                stopSelf(msg.arg1)
-            }
-            Log.d(TAG, "handleMessage" + msg.toString())
-            Log.d(TAG, "handleMessage " + msg.arg2)
-            // Normally we would do some work here, like download a file.
-            // For our sample, we just sleep for 5 seconds.
             try {
-                var i =0
-                while (true){
+                if(msg.obj != null){
 
-                    var arr :FloatArray? =model_test()
-                    model_test().let {
+                    var arr :FloatArray? =model_test(msg.obj as String)
 
-                        var cmd :String = "input tap "+String.format("%.1f", it?.get(0))+" "+String.format("%.1f", it?.get(1))
-                        //37 은 application에서 터치 이벤트 못받음 .....
-                        var cmd2 = "input tap 489.9 37.7"
+                    if(arr != null){
+                        Log.e(TAG, "res============model_test not null=========================" )
+                        var cmd :String = "input tap "+String.format("%.1f", arr!!.get(0))+" "+String.format("%.1f", arr!!.get(1))
                         Log.d(TAG, "adb -e shell " + cmd)
                         var sh_out= ShellExecuter().Executer(cmd)
-                        Log.d(TAG, "sh_out=====================================" + sh_out.toString())
+                    }else{
+                        Log.e(TAG, "res============model_test null=========================" )
                     }
 
-                    i= i+1
-                    Thread.sleep(500)
+
+
+                }else{
+                    Log.i(TAG, "res============handleMessage==== msg.obj null=====================" )
                 }
-             //   Log.d(TAG, "res=====================================" + res.toString())
 
-
-                Thread.sleep(500000)
+               // Thread.sleep(500000)
             } catch (e: InterruptedException) {
                 Thread.currentThread().interrupt()
             }
@@ -80,11 +111,10 @@ class BackgroundService : Service() {
     }
 
     override fun onCreate() {
-
-        Log.d(TAG, "onCreate====================================="+Run)
+        Log.e(TAG, "--------------BackgroundService --------onCreate----------------------")
 
         my_notify()
-        //my_media()
+        my_media()
 
         handlerThread = HandlerThread("서비스핸들러", Process.THREAD_PRIORITY_BACKGROUND)
         handlerThread!!.start()
@@ -93,18 +123,22 @@ class BackgroundService : Service() {
 
     }
 
+    private val mediaProjectionManager: MediaProjectionManager by lazy {
+        getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+    }
+
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        Log.d("backend", "빌드 -onStartCommand------------"+Run)
-        var arg2 = 0
-        var jobId =0
+        Log.e(TAG, "--------------BackgroundService --------onStartCommand---------Run=$Run,action=$my_action")
         if(Run){
-            arg2 = 1
-            jobId =1
-            serviceHandler?.obtainMessage()?.also { msg ->
-                msg.arg1 = jobId
-                msg.arg2 = arg2
-                serviceHandler?.sendMessage(msg)
+            if(my_action=="createVirtualDisplay"){
+                createVirtualDisplay()
+            }else if(my_action=="cap"){
+                serviceHandler?.obtainMessage()?.also { msg ->
+                    msg.obj = cap_filename
+                    serviceHandler?.sendMessage(msg)
+                }
             }
+
             Toast.makeText(this, "service starting~~~~~~~``", Toast.LENGTH_SHORT).show()
         }else{
             serviceHandler?.removeMessages(0)
@@ -121,11 +155,13 @@ class BackgroundService : Service() {
         return START_STICKY
     }
 
-    fun model_test(): FloatArray? {
+    fun model_test(cap_filename:String): FloatArray? {
         Log.d(TAG, "res=====================================model_test" )
         var run = Run(this)
         run.build()
-        return run.get_xy(RUN_IMG!!)
+        var res =  run.get_xy(cap_filename)
+        Log.d(TAG, "res=====================================model_test"+res.toString() )
+        return res
     }
 
 
@@ -134,14 +170,207 @@ class BackgroundService : Service() {
         noti!!.createNotificationChannel()
         var notify = noti!!.build(11232131);
         startForeground(FOREGROUND_SERVICE_ID, notify)
+        Log.e(TAG, "--------------BackgroundService --------my_notify----------------------Run="+Run)
     }
 
     fun my_media() {
-        mediaProjectionManager =
-            getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-        //startActivityForResult(mediaProjectionManager!!.createScreenCaptureIntent(), this)
+        my_file()
+        /*
+        1. 메인 액티비에서  서비스 실행
+        2. 서비스에서 권한 얻기 는 서브액티비에서.
+        3.  서브액티에서는
+        mediaProjectionManager.createScreenCaptureIntent()
+        찍고
+        4. 결과는 서비스로 전달달         */
+        //생성
+
+        //권환얻기=> 는 액티비티
         startActivity(MediaProjectionActivity.newInstance(this))
+        Log.e(TAG, "--------------BackgroundService --------my_media----------------------Run="+Run)
     }
+
+    fun my_file(){
+        val externalFilesDir = getExternalFilesDir(null)
+        if (externalFilesDir != null) {
+            STORE_DIRECTORY =
+                externalFilesDir.absolutePath + "/screenshots/"
+            val storeDirectory =
+                File(STORE_DIRECTORY)
+            if (!storeDirectory.exists()) {
+                val success: Boolean = storeDirectory.mkdirs()
+                if (!success) {
+                    Log.e(
+                        TAG,
+                        "failed to create file storage directory."
+                    )
+                    return
+                }
+            }
+        } else {
+            Log.e(
+                TAG,
+                "failed to create file storage directory, getExternalFilesDir is null."
+            )
+            return
+        }
+    }
+
+
+
+    private var IMAGES_PRODUCED = 0
+    var STORE_DIRECTORY: String? = null
+    val TAG: String = com.example.myapplication.MediaProjectionActivity::class.java.getName()
+
+    var mediaProjection: MediaProjection? = null
+    var mDisplay: Display? = null
+    var mVirtualDisplay: VirtualDisplay? = null
+    var mDensity = 0
+    var mWidth = 100
+    var mHeight = 100
+    var mRotation = 0
+    var mImageReader: ImageReader? = null
+    val SCREENCAP_NAME = "screencap"
+    val VIRTUAL_DISPLAY_FLAGS =
+        DisplayManager.VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY or DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC
+    var mHandler: Handler? = null
+    @SuppressLint("WrongConstant")
+    private fun createVirtualDisplay() {
+
+        val intent = Intent(applicationContext, MediaProjectionActivity::class.java)
+      //  val resultCode = intent.getIntExtra("", "")
+      //  val data = intent.getParcelableExtra<Intent>("request_data")
+        if(mediaProjectionManager == null){
+            Log.e(TAG, "mediaProjectionManager null~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~````````")
+        }
+        var my_mediaProjection = mediaProjectionManager.getMediaProjection(my_resultCode!!, my_data!!)
+        // start capture handling thread
+
+        // start capture handling thread
+        object : Thread() {
+            override fun run() {
+                Looper.prepare()
+                mHandler = Handler()
+                Looper.loop()
+            }
+        }.start()
+
+        Log.d(TAG, "1111111111111111111createVirtualDisplay1111111111111111111111")
+
+        // display metrics
+
+        // display metrics
+        val metrics = resources.displayMetrics
+        mDensity = metrics.densityDpi
+        var  windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        mDisplay = windowManager.getDefaultDisplay()
+
+
+
+        // get width and height
+        val size = Point()
+        mDisplay?.getSize(size)
+        mWidth = size.x
+        mHeight = size.y
+
+
+        // start capture reader
+        var my_mImageReader = ImageReader.newInstance(
+            mWidth,
+            mHeight,
+            PixelFormat.RGBA_8888,
+            2
+        )
+
+        var my_mVirtualDisplay = my_mediaProjection.createVirtualDisplay(
+            SCREENCAP_NAME,
+            mWidth,
+            mHeight,
+            mDensity,
+            VIRTUAL_DISPLAY_FLAGS,
+            my_mImageReader.getSurface(),
+            null,
+            mHandler
+        )
+
+        my_mImageReader.setOnImageAvailableListener(ImageAvailableListener(), mHandler)
+        Log.d(TAG, "22222222222222222222222222222222222")
+
+    }
+
+    inner class ImageAvailableListener : ImageReader.OnImageAvailableListener {
+
+
+        @RequiresApi(Build.VERSION_CODES.O)
+        override fun onImageAvailable(reader: ImageReader) {
+            Log.d(TAG, "-----------------------onImageAvailable----------------------------------")
+            var image: Image? = null
+            var fos: FileOutputStream? = null
+            var bitmap: Bitmap? = null
+            try {
+                image = reader.acquireLatestImage()
+                if (image != null) {
+                    val width = image.width
+                    val height = image.height
+                    val planes: Array<Image.Plane> = image.getPlanes()
+                    val buffer: ByteBuffer = planes[0].getBuffer()
+                    val pixelStride: Int = planes[0].getPixelStride()
+                    val rowStride: Int = planes[0].getRowStride()
+                    val rowPadding: Int = rowStride - pixelStride * mWidth
+
+                    // create bitmap
+                    bitmap = Bitmap.createBitmap(
+                        mWidth + rowPadding / pixelStride,
+                        mHeight,
+                        Bitmap.Config.ARGB_8888
+                    )
+                    bitmap.copyPixelsFromBuffer(buffer)
+
+                    // write bitmap to a file
+
+                    // write bitmap to a file
+                    var my_file = STORE_DIRECTORY + "myscreen_" + IMAGES_PRODUCED + ".JPEG"
+                    fos =
+                        FileOutputStream(my_file)
+                    bitmap.compress(CompressFormat.JPEG, 100, fos)
+                    //fos.close()
+                    //image.close()
+                    //reader.close()
+                    IMAGES_PRODUCED++
+                    Log.e(
+                        TAG,
+                        "captured image: " + my_file
+                    )
+
+                    Log.d(TAG, "-----------------------onImageAvailable----------------------------------"+my_file)
+                    startForegroundService(BackgroundService.newService(applicationContext,"cap",my_file))
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                if (fos != null) {
+                    try {
+                        fos.close()
+                    } catch (ioe: IOException) {
+                        ioe.printStackTrace()
+                    }
+                }
+                bitmap?.recycle()
+                if (image != null) {
+                    image.close()
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
 
     override fun onBind(intent: Intent): IBinder? {
         // We don't provide binding, so return null
