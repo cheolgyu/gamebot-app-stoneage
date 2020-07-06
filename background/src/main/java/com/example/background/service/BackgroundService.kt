db150b2808103352b3c2d0e1a5822160caf9879f
cheolgyu <rb7480@knou.ac.kr>
2020-07-06 18:11:43 +0900

    1차 됨.

--- background/src/main/java/com/example/background/service/BackgroundService.kt
+++ background/src/main/java/com/example/background/service/BackgroundService.kt
@@ -1,20 +1,34 @@
 package com.example.background.service
 
 //import com.example.tf.tflite.Run
+import android.annotation.SuppressLint
 import android.app.Service
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.PixelFormat
+import android.graphics.Point
+import android.media.Image
+import android.media.ImageReader
 import android.media.projection.MediaProjectionManager
 import android.os.IBinder
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.WindowManager
 import android.widget.Toast
 import com.example.background.notification.Noti
 import java.io.File
+import java.io.FileOutputStream
+import java.nio.ByteBuffer
 
 
 class BackgroundService : Service() {
     companion object {
+        var imageReader: ImageReader? = null
+        var mWidth: Int? = null
+        var mHeight: Int? = null
+
         var Run = false
         private val FOREGROUND_SERVICE_ID = 1000
         private var cap_filename: String? = null
@@ -59,6 +73,11 @@ class BackgroundService : Service() {
         getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
     }
 
+    private val windowManager: WindowManager by lazy {
+        getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    }
+
+    @Throws(java.lang.Exception::class)
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         Log.e(
             TAG,
@@ -75,12 +94,43 @@ class BackgroundService : Service() {
                 구한결과 접근성.터치 로 실행.
 
                  */
-                var windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+                make_image_reader()
+
                 BackgroundServiceMP(
                     mediaProjectionManager,
                     windowManager.defaultDisplay,
+                    imageReader!!,
                     STORE_DIRECTORY!!
                 ).createVirtualDisplay()
+
+                // start capture handling thread
+                object : Thread() {
+                    override fun run() {
+                        while (true){
+                            Log.e(
+                                "쓰레드",
+                                "--------------------------------------------"
+                            )
+                            var file = image_available()
+                            if (file != null) {
+                                var arr : FloatArray =  tflite_run(file)
+                                if(arr.size >0){
+                                    var x = arr.get(0)
+                                    var y = arr.get(1)
+
+                                    touchService!!.click(x,y)
+                                }
+                            }else{
+                                Log.e(
+                                    "쓰레드",
+                                    "image_available null "
+                                )
+                            }
+                        }
+                    }
+                }.start()
+
             }
             Toast.makeText(this, "service starting~~~~~~~``", Toast.LENGTH_SHORT).show()
         } else {
@@ -91,19 +141,84 @@ class BackgroundService : Service() {
         return START_STICKY
     }
 
-//    fun tflite_run(cap_filename: String): FloatArray? {
-//        Log.d(TAG, "res=====================================tflite_run")
-//        var run = Run(this)
-//        run.build()
-//        var res = run.get_xy(cap_filename)
-//        Log.d(TAG, "res=====================================modetflite_runl_test" + res.toString())
-//        var cmd: String = "input tap " + String.format("%.1f", res!!.get(0)) + " " + String.format(
-//            "%.1f",
-//            res!!.get(1)
-//        )
-//        return null
-//        // return res
-//    }
+    fun image_available(): String? {
+        var image = imageReader!!.acquireLatestImage()
+        if (image != null) {
+            var fos: FileOutputStream? = null
+            var IMAGES_PRODUCED = 0
+            val planes: Array<Image.Plane> = image.getPlanes()
+            val buffer: ByteBuffer = planes[0].getBuffer()
+            val pixelStride: Int = planes[0].getPixelStride()
+            val rowStride: Int = planes[0].getRowStride()
+            val rowPadding: Int = rowStride - pixelStride * mWidth!!
+
+            // create bitmap
+            var bitmap = Bitmap.createBitmap(
+                mWidth!! + rowPadding / pixelStride,
+                mHeight!!,
+                Bitmap.Config.ARGB_8888
+            )
+            bitmap.copyPixelsFromBuffer(buffer)
+            image.close()
+            // write bitmap to a file
+
+            // write bitmap to a file
+            var my_file = STORE_DIRECTORY + "myscreen_" + IMAGES_PRODUCED + ".JPEG"
+            fos =
+                FileOutputStream(my_file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+
+            IMAGES_PRODUCED++
+            Log.e(
+                ContentValues.TAG,
+                "captured image: " + my_file
+            )
+
+            Log.d(
+                ContentValues.TAG,
+                "-----------------------onImageAvailable----------------------------------" + my_file
+            )
+            return my_file
+
+        }
+        return null
+    }
+
+    @SuppressLint("WrongConstant")
+    fun make_image_reader() {
+        // display metrics
+        val metrics = DisplayMetrics()
+        var display = windowManager.defaultDisplay
+        display!!.getMetrics(metrics)
+
+        // get width and height
+        val size = Point()
+        display!!.getSize(size)
+         mWidth = size.x
+         mHeight = size.y
+
+        // start capture reader
+        imageReader = ImageReader.newInstance(
+            mWidth!!,
+            mHeight!!,
+            PixelFormat.RGBA_8888,
+            2
+        )
+    }
+
+    fun tflite_run(cap_filename: String): FloatArray {
+        Log.d(TAG, "res=====================================tflite_run")
+        var run = com.example.tf.tflite.Run(this)
+        run.build()
+        var res = run.get_xy(cap_filename)
+        Log.d(TAG, "res=====================================modetflite_runl_test" + res.toString())
+        var cmd: String = "input tap " + String.format("%.1f", res!!.get(0)) + " " + String.format(
+            "%.1f",
+            res!!.get(1)
+        )
+        //return null
+        return res
+    }
 
 
     fun run_notify() {
