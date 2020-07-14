2f0b192113c0e9b4bf94da95f1902f602744ff92
cheolgyu <rb7480@knou.ac.kr>
2020-07-14 14:47:52 +0900

    접근성체크

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,25 +1,23 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.MotionEvent
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.background.CheckTouch
 import com.example.background.service.BackgroundService
 
 
-class MainActivity : AppCompatActivity()  , View.OnClickListener{
+class MainActivity : AppCompatActivity()  {
     private val ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        var chk = CheckTouch(this)
-        if(!chk.checkAccessibilityPermissions()){
-            chk.setAccessibilityPermissions()
+        if(!CheckTouch(this).chk()){
+
+        }else{
+
         }
        // start_touch_service_btn()
         val action = intent.extras?.getString("action")
@@ -29,108 +27,23 @@ class MainActivity : AppCompatActivity()  , View.OnClickListener{
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val  button7 =
-            findViewById<View>(R.id.button7) as Button
-        button7.setOnClickListener {
-            Toast.makeText(
-                applicationContext,
-                "222222222222222", Toast.LENGTH_SHORT
-            ).show()
-        }
-
-
-
-        val button2 =
-            findViewById<View>(R.id.button2) as Button
-        button2.setOnClickListener {
-            Toast.makeText(
-                applicationContext,
-                "222222222222222", Toast.LENGTH_SHORT
-            ).show()
-        }
-        val button3 =
-            findViewById<View>(R.id.button3) as Button
-        button3.setOnClickListener {
-            Toast.makeText(
-                applicationContext,
-                "33333333333333", Toast.LENGTH_SHORT
-            ).show()
-        }
-
-        val button4 =
-            findViewById<View>(R.id.button4) as Button
-        button4.setOnClickListener {
-            Toast.makeText(
-                applicationContext,
-                "4444444444444444444", Toast.LENGTH_SHORT
-            ).show()
-        }
-
-    }
-
-
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        if (event != null) {
-
-
-                val x = event.getX()
-                val y = event.getY()
-
-            Log.d("터치를 입력받음 ",x.toString()+" / " +y)
-                //Toast. makeText(applicationContext, msg, Toast.LENGTH_SHORT ).show();
-                return true;
-        }
-
-
-        return super.onTouchEvent(event)
-    }
-    fun showMessage(view: View?) {
-        Toast.makeText(this, "1111111111111111111", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
-        Toast.makeText(this, "0000000000000000000", Toast.LENGTH_SHORT).show();
-    }
-
-    var Button4OnClick =
-        View.OnClickListener {
-            Toast.makeText(
-                applicationContext,
-                "Button4 Clicked!", Toast.LENGTH_SHORT
-            ).show()
-        }
-
-    fun move_btn_demo(view: View?){
-        val intent = Intent(applicationContext, com.example.background.MediaProjectionActivity::class.java)
-        startActivity(intent)
     }
 
     fun service_start_btn(view: View?) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        if(!CheckTouch(this).chk()){
+            Toast.makeText( applicationContext, "접근성 권한필요해요", Toast.LENGTH_SHORT).show()
+        }else{
             service_action("start")
-        } else {
-            service_action("start")
-            Log.e("sdfafasd","버전맞춰!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         }
+
     }
 
     fun service_stop_btn(view: View?) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            service_action("stop")
-        } else {
-            Toast.makeText(
-                applicationContext,
-                "stop !", Toast.LENGTH_SHORT
-            ).show()
-        }
+        service_action("stop")
     }
 
     private fun service_action(action:String) {
-        Toast.makeText(
-            applicationContext,
-            "service_action-Build.VERSION.SDK_INT="+Build.VERSION.SDK_INT, Toast.LENGTH_SHORT
-        ).show()
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             startForegroundService(BackgroundService.newService(this,action))
             Toast.makeText(
