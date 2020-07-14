3d89dddf640ff5c42bb11e354ebea281aad8c555
cheolgyu <rb7480@knou.ac.kr>
2020-07-14 14:47:52 +0900

    접근성체크

--- background/src/main/java/com/example/background/CheckTouch.kt
+++ background/src/main/java/com/example/background/CheckTouch.kt
@@ -1,27 +1,37 @@
 package com.example.background
 
 import android.accessibilityservice.AccessibilityServiceInfo
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.provider.Settings
+import android.util.Log
 import android.view.accessibility.AccessibilityManager
 
 
+@SuppressLint("ServiceCast")
 class CheckTouch(val context: Context) {
+    val am: AccessibilityManager by lazy {
+        context.getSystemService(Context.WINDOW_SERVICE) as AccessibilityManager
+    }
+    fun chk(): Boolean {
+        if(!checkAccessibilityPermissions()){
+            setAccessibilityPermissions()
+        }else{
+            return true
+        }
+        return false
+    }
 
     fun checkAccessibilityPermissions(): Boolean {
-        val accessibilityManager =
-            context.getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager?
 
-        // getEnabledAccessibilityServiceList는 현재 접근성 권한을 가진 리스트를 가져오게 된다
         val list =
-            accessibilityManager!!.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.DEFAULT)
+            am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK)
         for (i in list.indices) {
             val info = list[i]
-
-            // 접근성 권한을 가진 앱의 패키지 네임과 패키지 네임이 같으면 현재앱이 접근성 권한을 가지고 있다고 판단함
+            Log.d("-----",info.resolveInfo.serviceInfo.packageName )
             if (info.resolveInfo.serviceInfo.packageName == context.packageName) {
                 return true
             }
@@ -29,13 +39,12 @@ class CheckTouch(val context: Context) {
         return false
     }
 
-    // 접근성 설정화면으로 넘겨주는 부분
     fun setAccessibilityPermissions() {
         val gsDialog: AlertDialog.Builder = AlertDialog.Builder(context)
         gsDialog.setTitle("접근성 권한 설정")
         gsDialog.setMessage("접근성 권한을 필요로 합니다")
         gsDialog.setPositiveButton("확인",
-            DialogInterface.OnClickListener { dialog, which -> // 설정화면으로 보내는 부분
+            DialogInterface.OnClickListener { dialog, which ->
                 context.startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
                 return@OnClickListener
             }).create().show()
