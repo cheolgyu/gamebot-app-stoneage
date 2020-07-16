2b2fb0030fa6df97a96caf66678533ffddc45e48
cheolgyu <rb7480@knou.ac.kr>
2020-07-17 00:12:22 +0900

    코드포맷

--- background/src/main/java/com/example/background/MediaProjectionActivity.kt
+++ background/src/main/java/com/example/background/MediaProjectionActivity.kt
@@ -17,48 +17,49 @@ open class MediaProjectionActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        if(CheckTouch(this).chk()){
-            msg="접근성 권한을 얻었습니다."
+        if (CheckTouch(this).chk()) {
+            msg = "접근성 권한을 얻었습니다."
             textView2.setText(msg)
-        }else{
+        } else {
             textView2.setText("접근성 권한이 필요해요.")
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mediaprojection)
         textView2.setText(msg)
 
         val action = intent.extras?.getString("action")
-        if ( action!=null && action =="stop" ){
-            startService(BackgroundService.newService(this,"stop"))
+        if (action != null && action == "stop") {
+            startService(BackgroundService.newService(this, "stop"))
         }
 
     }
 
     fun service_stop_btn(view: View?) {
-        startService(BackgroundService.newService(this,"stop"))
+        startService(BackgroundService.newService(this, "stop"))
     }
 
     fun service_start_btn(view: View?) {
-        if(CheckTouch(this).chk()){
-            msg="접근성 권한을 얻었습니다."
+        if (CheckTouch(this).chk()) {
+            msg = "접근성 권한을 얻었습니다."
             textView2.setText(msg)
             var mediaProjectionManager =
                 getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
             var captureIntent: Intent = mediaProjectionManager.createScreenCaptureIntent()
             startActivityForResult(captureIntent, 1000)
-        }else{
-            msg="접근성 권한이 필요해요."
+        } else {
+            msg = "접근성 권한이 필요해요."
             textView2.setText(msg)
-            Toast.makeText( applicationContext, "접근성 권한이 필요해요.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "접근성 권한이 필요해요.", Toast.LENGTH_SHORT).show()
 
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (resultCode == Activity.RESULT_OK) {
-            startService(BackgroundService.newService(this,"start",resultCode,data!!))
+            startService(BackgroundService.newService(this, "start", resultCode, data!!))
         } else {
             Log.e("", "=================else====================")
         }
