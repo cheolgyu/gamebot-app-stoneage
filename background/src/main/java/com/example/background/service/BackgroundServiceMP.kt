86db8835e9c76ec772f638ae4fa02f438b2013db
cheolgyu <rb7480@knou.ac.kr>
2020-07-17 06:28:01 +0900

    api 25 에서 회전후 window매니져에서 가로세로 바뀐것을 못가져옴. 18 접근성서비스의 캡쳐로 가야할듯

--- background/src/main/java/com/example/background/service/BackgroundServiceMP.kt
+++ background/src/main/java/com/example/background/service/BackgroundServiceMP.kt
@@ -1,7 +1,9 @@
 package com.example.background.service
 
 import android.annotation.SuppressLint
+import android.app.Service
 import android.content.Context
+import android.content.Intent
 import android.graphics.PixelFormat
 import android.graphics.Point
 import android.hardware.display.DisplayManager
@@ -11,26 +13,89 @@ import android.media.ImageReader
 import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
 import android.os.Handler
+import android.os.IBinder
 import android.os.Looper
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.OrientationEventListener
+import android.view.Surface
 import android.view.WindowManager
+import android.widget.Toast
 
 
-class BackgroundServiceMP(
-    val context: Context,
-    val mediaProjectionManager: MediaProjectionManager,
-    val wm: WindowManager
-) {
+abstract class BackgroundServiceMP : Service() {
+    var RUN_BACKGROUND = false
+    var my_data: Intent? = null
+    var my_resultCode: Int? = null
+
+    //display
+    var mWidth = 0
+    var mHeight = 0
+    var mRotation = 0
+    var mDensity = 0
+    var imageReader: ImageReader? = null
 
     var mProjectionStopped = true
-    var mDensity = 0
     val SCREENCAP_NAME = "screencap"
     val VIRTUAL_DISPLAY_FLAGS =
         DisplayManager.VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY or DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC or VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR
     var mHandler: Handler? = null
     var virtualDisplay: VirtualDisplay? = null
     var mediaProjection: MediaProjection? = null
+    val mediaProjectionManager: MediaProjectionManager by lazy {
+        getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+    }
+    val windowManager: WindowManager by lazy {
+        getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    }
+
+    fun set_display() {
+        val displaymetrics = DisplayMetrics()
+        val wm = windowManager
+        wm.getDefaultDisplay().getMetrics(displaymetrics)
+
+        val screenDensityDpi = displaymetrics.densityDpi
+        val screenRotation = getScreenOrientation()
+
+        val size = Point()
+        wm.getDefaultDisplay().getRealSize(size)
+        val screenWidth = size.x
+        val screenHeight = size.y
+
+        mWidth = screenWidth
+        mHeight = screenHeight
+        mDensity = screenDensityDpi
+        mRotation = getScreenOrientation()
+        Log.d("chk_display!!22", "$screenRotation")
+        chk_display()
+    }
+
+    fun chg_xy() {
+        Log.d("chk_display!!~~~~sss", "$mWidth,$mHeight")
+        var tmp = mWidth
+        mWidth = mHeight
+        mHeight = tmp
+        Log.d("chk_display!!~~~~eee", "$mWidth,$mHeight")
+    }
+
+    fun getScreenOrientation(): Int {
+
+        val wm = applicationContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        return when (wm.getDefaultDisplay().getRotation()) {
+            Surface.ROTATION_270 -> 270
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_90 -> 90
+            else -> 0
+        }
+    }
+
+    fun chk_display() {
+        var str = "mWidth=$mWidth,"
+        str += "mHeight=$mHeight,"
+        str += "mRotation=$mRotation,"
+        Log.d("---chk_display-------", str)
+    }
+
 
     @SuppressLint("WrongConstant")
     fun createVirtualDisplay() {
@@ -49,6 +114,8 @@ class BackgroundServiceMP(
                 Looper.loop()
             }
         }.start()
+        set_display()
+        make_image_reader()
 
         virtualDisplay = get_virtualDisplay()!!
         mProjectionStopped = false
@@ -66,11 +133,11 @@ class BackgroundServiceMP(
 
         return mediaProjection!!.createVirtualDisplay(
             SCREENCAP_NAME,
-            BackgroundService.mWidth!!,
-            BackgroundService.mHeight!!,
+            mWidth,
+            mHeight,
             mDensity,
             VIRTUAL_DISPLAY_FLAGS,
-            BackgroundService.imageReader!!.surface,
+            imageReader!!.surface,
             null,
             mHandler
         )
@@ -78,25 +145,10 @@ class BackgroundServiceMP(
 
     @SuppressLint("WrongConstant")
     fun make_image_reader() {
-
-
-        var metrics = DisplayMetrics()
-        var display = wm.defaultDisplay
-        wm.defaultDisplay.getMetrics(metrics)
-        mDensity = metrics.densityDpi
-
-        display!!.getMetrics(metrics)
-        BackgroundService.mRotation = wm!!.getDefaultDisplay().getRotation()
-        // get width and height
-        var size = Point()
-        display.getRealSize(size)
-        BackgroundService.mWidth = size.x
-        BackgroundService.mHeight = size.y
-        var aa = size.toString()
         // start capture reader
-        BackgroundService.imageReader = ImageReader.newInstance(
-            BackgroundService.mWidth!!,
-            BackgroundService.mHeight!!,
+        imageReader = ImageReader.newInstance(
+            mWidth,
+            mHeight,
             PixelFormat.RGBA_8888,
             2
         )
@@ -105,17 +157,31 @@ class BackgroundServiceMP(
     inner class OrientationChangeCallback internal constructor(
 
     ) :
-        OrientationEventListener(context) {
+        OrientationEventListener(this) {
         override fun onOrientationChanged(orientation: Int) {
-            val display = wm.defaultDisplay
-            val rotation: Int = display.getRotation();
-            if (rotation != BackgroundService.mRotation) {
+            chk_display()
+            Log.d("chk_display22-onOrientationChanged","$orientation")
+            if(mRotation == 0 || mRotation == 180){
+                if (orientation == 270 || orientation == 90) {
+                    mRotation = 270
+                    chg_xy()
+                }
+            }else{
+                if (orientation == 0 || orientation == 180) {
+                    mRotation = 0
+                    chg_xy()
+                }
+            }
+
+
+            Thread.sleep(100)
+            if (orientation != mRotation) {
 
                 if (virtualDisplay != null) {
                     virtualDisplay!!.release()
                 }
-                if (BackgroundService.imageReader != null) {
-                    BackgroundService.imageReader!!.setOnImageAvailableListener(null, null)
+                if (imageReader != null) {
+                    imageReader!!.setOnImageAvailableListener(null, null)
                 }
                 if (!mProjectionStopped) {
                     virtualDisplay = get_virtualDisplay()!!
@@ -126,6 +192,26 @@ class BackgroundServiceMP(
 
     }
 
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun onDestroy() {
+        Log.d("", "onDestroy")
+        RUN_BACKGROUND = false
+        Toast.makeText(this, "service done onDestroy", Toast.LENGTH_SHORT).show()
+    }
+
+    fun stopForegroundService() {
+        Log.d("", "Stop foreground service.")
+        Toast.makeText(this, "service done stopForegroundService", Toast.LENGTH_SHORT).show()
+        // Stop foreground service and remove the notification.
+        stopForeground(true)
+
+        // Stop the foreground service.
+        stopSelf()
+    }
 
 }
 
